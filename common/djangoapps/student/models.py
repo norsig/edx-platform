512d3ee4311925ab0bd4b039cd2ec74841dd44dc
ayub-khan <muhammadayubkhan6@gmail.com>
2018-06-27 12:34:03 +0500

    Updated intructor unenroll If student is in refund window, refund student, update mode to audit, and set is_active false. if student is not in refund window, do not refund student do not change mode, and set is_active to false.

--- common/djangoapps/student/models.py
+++ common/djangoapps/student/models.py
@@ -1569,7 +1569,11 @@ class CourseEnrollment(models.Model):
         """
         try:
             user = User.objects.get(email=email)
-            return cls.unenroll(user, course_id)
+            record = cls.objects.get(user=user, course_id=course_id)
+            if record.refundable():
+                return record.update_enrollment(mode=CourseMode.AUDIT, is_active=False, skip_refund=False)
+            else:
+                return record.update_enrollment(is_active=False, skip_refund=True)
         except User.DoesNotExist:
             log.error(
                 u"Tried to unenroll email %s from course %s, but user not found",
